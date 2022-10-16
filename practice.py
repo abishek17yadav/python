47d6540d9ecf9d1502d3949d3b554817f1b400aa
Abishek_yadav <abhiyadav172408@gmail.com>
2022-10-16 13:17:21 +0530

    abishek

---
+++ practice.py
@@ -0,0 +1,9 @@
+
+from __future__ import division
+from audioop import add
+
+
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+answer=num1/num2
+print(answer)

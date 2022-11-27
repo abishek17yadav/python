0fc0f873abbf6818bbfdbad5e87ff189780fd493
Abishek_yadav <abhiyadav172408@gmail.com>
2022-11-27 09:21:00 +0530

    opp

---
+++ operators.py
@@ -0,0 +1,19 @@
+#assignment operator
+# a=34
+# a*=2
+# print(a)
+# #you can do anything just change the sign
+
+# #comparison operator
+# b=(5>7)
+# print(b)
+# #it will print true or false
+
+#logical operator
+bol1= True
+bol2= False
+print("the value of bol1 and bol2 is ",(bol1 and bol2))
+print("the value of bol1 or bol2 is ",(bol1 or bol2))
+print("the value of bol1 not bol2 is ",( not bol2))
+
+#not  - ulta kardeta hai true ko false and sop on

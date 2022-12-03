6469f5015009d427746f5e33a7947727738115fa
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-03 21:07:39 +0530

    typecasting

---
+++ 03_typecasting.py
@@ -0,0 +1,6 @@
+# a = "333"
+# a=int(a) #convert a to integer
+# print(a+6)
+
+# type casting is used to change string to int and etc..str,float,int 
+# type casting to cange a data type to another data dype is type casting

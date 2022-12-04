c69c845bed989b434e81f4cc0e4242f50481a67f
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-04 12:14:39 +0530

    strings

---
+++ 04strings.py
@@ -0,0 +1,19 @@
+'''#string slicing
+name = "abishek"
+#concaneting two strings
+print(name[0])
+
+# string slicing
+b="rahul"
+print(b[0:3])
+
+#you ca print using negative(-) number also
+print(b[-1])'''
+
+'''# slicing with skip value
+d="abishek"
+e=d[1:4:2]#here 2 is skipped 
+print(e)
+
+f= d[0::3]# it will skip 2 values
+print(f)'''

96468b1612c8920b0d3938fc69f311009c1b38ee
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-05 21:25:32 +0530

    string

---
+++ 05string_function.py
@@ -0,0 +1,19 @@
+'''story="once upon a time there was a youtuber named harry who uploded python course with notes"
+
+#string functions
+print(len(story))
+
+#string.endswith
+print(story.endswith("notes"))# if yes then it will print true orelse false
+
+#string.count
+print(story.count("o"))#it will count how many o are there #,you count any thing even words also just put the name
+
+#string.capatalize
+print(story.capitalize())  # it will capatalise the first word of the string
+
+#string.find
+print(story.find("harry")) #it will find that where harry is and say the index
+
+#string.replace
+print(story.replace("harry","abishek"))# it will replace with one to one'''

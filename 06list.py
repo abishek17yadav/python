6ba921f98b4a9b185b24253375966ff180187328
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-06 21:11:17 +0530

    list

---
+++ 06list.py
@@ -0,0 +1,71 @@
+'''#list : to store a sset a value of any data type & they are ordered
+a = [1,2,4,56,6]
+print (a)
+
+#to print 1st value and .........same do for others 
+print(a[0])
+
+#to change any value from the list & ...........same do for others
+a[0]=99
+print (a)
+
+#we can create a list with items of different types
+c = [45,"harry",False,6.9]
+print(c)
+
+#list slicing
+friends=[1,2,"r","t" "y"]
+print(friends[0:3])'''
+
+
+
+'''#methods in list
+
+#sorting    -- arranging in a accending wise
+a=[1,8,7,3,15]
+#a.sort()
+#print (a)
+
+Revesre   - it will reverse the list
+a.reverse()
+print (a)
+
+#Append   -adds at the end of the list
+# a.append(17)
+# a.append(24)
+# print (a)
+
+#insert  - inserts 444 at index 0  
+# a.insert(0,444)
+# print (a)
+
+#pop  deletes the number of the given list at a specific index
+# a.pop(2)
+# print (a)
+
+#remove  --removes the number of the list
+# a.remove(8)
+# print (a)
+
+#add  -to add the numbers in the list
+print(a[0]+a[1]+a[2]+a[3]+a[4])
+Revesre   - it will reverse the list
+a.reverse()
+print (a)
+#or 
+print(sum(a))'''
+# reverse   - to revesre the sidt
+# a = [1,2,4,56,6]
+# a.reverse
+# print (a)
+
+
+
+# list1=[10,20,30,40,50]
+# a=list1*3  #to print 3 times
+# print(a)
+
+
+# list2=[34,88,30,22,10,15,18]
+# output=list(filter(lambda i : i%5==0,list2))  #to print the multiple of 5
+# print(output)

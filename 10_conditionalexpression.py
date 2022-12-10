ed7e06bc6be5fb24c6354cacdc1c199f68fc831e
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-10 16:26:34 +0530

    conditionalexpression

---
+++ 10_conditionalexpression.py
@@ -0,0 +1,44 @@
+#if else or elip in python  -are multi way decision taken by our program due to certain conditions in our code
+'''a=45
+if (a>3):
+    print("the value of a is greater than 3") #tab or 4spaces after if to print anything
+elif (a>7):
+    print ("the value of a is greater than 7")
+else:
+    print("the value is not greater thsn 3 or 7")  '''
+
+
+''' # age proble
+age =int(input("enter:"))    
+if (age>18):
+    print("yes")
+elif (age<18):
+    print("no")
+else:
+    print("invalid")  '''      
+
+
+
+'''#relational operator   (,==,<=,>=,)etc..
+age =int(input("enter:"))    
+if (age==18):
+    print("yes")
+elif (age<18):
+    print("no")
+else:
+    print("invalid")'''   
+
+
+'''#logical operator  (and ,or ,not)   
+#AND  - both should be true here
+age =int(input("enter")) 
+if (age>34 and age<56):
+    print("yes allowed")
+else:
+    print("invalid")  '''
+
+
+#or    any one should be true
+'''age =int(input("enter")) 
+if (age>34 or age<56):
+    print("yes allowed") '''

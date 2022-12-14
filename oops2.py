f956d10f9ea779ad17d352457f506484a5267880
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-14 21:45:58 +0530

    oops

---
+++ oops2.py
@@ -0,0 +1,80 @@
+# class laptop:
+#     def __init__(self):
+#         self.name="Asus"
+#         self.processor="r7"
+#     def printoutput(self):
+#         print("my laptop is :",self.name,"and processor is :",self.processor)
+
+#     # def config(self):
+#     #     print("asus","r7","g11")
+# laptop1=laptop()
+# laptop2=laptop()
+
+# laptop1.printoutput()
+# laptop2.printoutput()
+
+
+
+# class laptop:
+#     def __init__(self ,name,processor):
+#         self.name=name
+#         self.processor=processor
+#     def printoutput(self):
+#         print("my laptop is :",self.name,"and processor is :",self.processor)
+
+#     # def config(self):
+#     #     print("asus","r7","g11")
+# laptop1=laptop("asus","r7")
+# laptop2=laptop("dell" ,"i3")
+
+# laptop1.printoutput()
+# laptop2.printoutput()
+
+
+# class student:
+#     def __init__(self,name,rollno):
+#         self.name=name
+#         self.rollno=rollno
+#     def printout(self) :
+#         print("my name is :",self.name,"and my roll no is :",self.rollno) 
+
+# student1=student("abishek","29")                 
+# student2=student("bishal","19")    
+
+# student1.printout()
+# student2.printout()
+
+#instance:
+# class person:
+#     def __init__(self):
+#         self.name="abishek"
+#         self.age=17
+#     def updatename(self,name):
+#         self.name=name 
+# person1=person()       
+# person2=person() 
+
+# person1.updatename("rahul")
+
+# print(person1.name)
+# print(person2.name)
+
+# def compareage(self,other):
+#     if self.age==other.age:
+#         return True
+#     else:
+#         return False
+
+
+  
+
+
+
+
+
+
+
+
+
+
+

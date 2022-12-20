bb17a225c3358b8595b8ca5fac636d38c711ce82
Abishek_yadav <abhiyadav172408@gmail.com>
2022-12-20 23:50:41 +0530

    oop3

---
+++ 19_oop_.py
@@ -0,0 +1,44 @@
+# class -is a blueprint of a object
+# object-is a instance of a class
+
+
+
+# class RailwayForm:
+#     formType="railwayForm"
+#     def printData(self):
+#         print(f"name is {self.name} train is {self.train}")
+
+# abishekapplication=RailwayForm()
+# abishekapplication.name="abishek"
+# abishekapplication.train="andaman express"
+# abishekapplication.printData()
+
+
+# class employee:
+#     company="google"
+
+# abishek=employee()
+# rahul=employee()
+# abishek.salary=500
+# rahul.salary=399
+# print(abishek.company)
+# print(rahul.company)
+# employee.company="microsoft"  #if u want to change the company
+# print(abishek.company)
+# print(rahul.company)
+# print(abishek.salary)
+# print(rahul.salary)
+
+
+
+#self-paramet jo pass hota hai automaticall for an object
+
+#static method
+@staticmethod
+def greet():
+    print("good morning abishek")
+
+greet()
+
+
+

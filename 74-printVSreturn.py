ebde43d5fd16161a7742e222e8de0586d97dde89
Abishek yadav <abhiyadav172408@gmail.com>
2023-01-12 20:03:29 +0530

    print vs return

---
+++ 74-printVSreturn.py
@@ -0,0 +1,8 @@
+def addThree(n1,n2,n3):
+    return n1+n2+n3
+
+a=int(int(input("enter the number: ")))
+b=int(int(input("enter the number: ")))
+c=int(int(input("enter the number: ")))
+
+print(addThree(a,b,c))
